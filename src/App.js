7613c3cb0320646c8c5b2932368ec37829a3ffd9
Jason <Jason@MacBook-Pro-3.local>
2020-01-29 22:49:00 -0800

    Added reset button for scores...  :(

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore,setHomeScore] = useState(0)
 const [awayScore,setAwayScore] = useState(0)
+const reset = function (){
+   setHomeScore(0)
+   setAwayScore(0)
+}
 
 console.log(homeScore)
-
 console.log(awayScore)
+
 return (
     <div className="container">
       <section className="scoreboard">
@@ -35,6 +39,9 @@ return (
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown"onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal"onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
+        </div>
+        <div>
+          <button className=''onClick={() => reset()}>RESET SCORE!</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown"onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
