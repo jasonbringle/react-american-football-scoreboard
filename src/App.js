5cae68f0c6efd5da6ee179da0f834429478448e5
Jason <Jason@MacBook-Pro-3.local>
2020-01-30 22:02:49 -0800

    MVP

--- src/App.js
+++ src/App.js
@@ -1,70 +1,108 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
-
-
+import Timer from "./Timer";
 
 function App() {
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
 
-  const [homeScore,setHomeScore] = useState(0)
-  const [awayScore,setAwayScore] = useState(0)
-  const reset = function (){
-      setHomeScore(0)
-      setAwayScore(0)
-  }
-    
+  const reset = function() {
+    setHomeScore(0);
+    setAwayScore(0);
+  };
 
-  function Home(){
-    return <div className="home">
+  const quarterAdd = function() {
+    if (quarter < 4) {
+      return quarter + 1;
+    } else {
+      return quarter - 3;
+    }
+  };
+
+  return (
+    <div className="container">
+      <section className="scoreboard">
+        <div className="topRow">
+          <div className="home">
             <h2 className="home__name">Lions</h2>
             <div className="home__score">{homeScore}</div>
           </div>
-    }
-
-  function Away(){
-    return <div className="away">
+          <Timer />
+          <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
           </div>
-  }
-
-  function Buttons(){
-    return <section className="buttons">
-      <div className="homeButtons">
-        <button className="homeButtons__touchdown"onClick={() => setHomeScore(homeScore + 7)}       >Home Touchdown</button>
-        <button className="homeButtons__fieldGoal"onClick={() => setHomeScore(homeScore + 3)}       >Home Field Goal</button>
-      </div>
-
-      <div className="awayButtons">
-        <button className="awayButtons__touchdown"onClick={() => setAwayScore(awayScore + 7)}       >Away Touchdown</button>
-        <button className="awayButtons__fieldGoal"onClick={() => setAwayScore(awayScore + 3)}       >Away Field Goal</button>
-      </div>
-
-      <div className = 'resetButton'>
-        <button className='reset__button'onClick={() => reset()}>RESET SCORE!</button>
-      </div>
-
-      </section>
-  }
-
-  function Timer(){
-    return <div className="timer">00:03</div>
-  }
-
-  return (
-      <div className="container">
-        <section className="scoreboard">
-          <div className="topRow">
-            <Home />
-              <Timer />
-                <Away />
+        </div>
+        <div className="bottomRow">
+          <div className="down">
+            <h3 className="down__title">Down</h3>
+            <div className="down__value">3</div>
           </div>
-          <BottomRow />
-        </section>
-        <Buttons />
-      </div>
-    );
+          <div className="toGo">
+            <h3 className="toGo__title">To Go</h3>
+            <div className="toGo__value">7</div>
+          </div>
+          <div className="ballOn">
+            <h3 className="ballOn__title">Ball on</h3>
+            <div className="ballOn__value">21</div>
+          </div>
+          <div className="quarter">
+            <h3 className="quarter__title">Quarter</h3>
+            <div className="quarter__value">{quarter}</div>
+          </div>
+        </div>
+      </section>
+      <section className="buttons">
+        <div className="homeButtons">
+          <button
+            className="homeButtons__touchdown"
+            onClick={() => setHomeScore(homeScore + 7)}
+          >
+            Home Touchdown
+          </button>
+          <button
+            className="homeButtons__fieldGoal"
+            onClick={() => setHomeScore(homeScore + 3)}
+          >
+            Home Field Goal
+          </button>
+        </div>
+
+        <div className="awayButtons">
+          <button
+            className="awayButtons__touchdown"
+            onClick={() => setAwayScore(awayScore + 7)}
+          >
+            Away Touchdown
+          </button>
+          <button
+            className="awayButtons__fieldGoal"
+            onClick={() => setAwayScore(awayScore + 3)}
+          >
+            Away Field Goal
+          </button>
+        </div>
+
+        <div className="resetButton">
+          <button className="reset__button" onClick={() => reset()}>
+            RESET SCORE!
+          </button>
+        </div>
+
+        <div className="quarterButton">
+          <button
+            className="quarter__title"
+            onClick={() => setQuarter(quarterAdd())}
+          >
+            ADD A QUARTER
+          </button>
+        </div>
+      </section>
+      }
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Timer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "./App.js";
+
+function Timer(){
+    return <div className="timer">00:03</div>
+  }
+
+  export default Timer;
